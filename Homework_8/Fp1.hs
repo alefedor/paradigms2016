fcdae4074adc693bea9bb4d696b13499ba32a861
studentfedorov <fedorov2602>
2016-11-11 22:07:01 +0300

    Added hw 8

---
+++ Homework_8/Fp1.hs
@@ -0,0 +1,38 @@
+head' :: [a] -> a
+head' (x:xs) = x
+
+tail' :: [a] -> [a]
+tail' [] = []
+tail' (x:xs) = xs
+
+take' :: Int -> [a] -> [a]
+take' 0 xs = []
+take' n [] = []
+take' n (x:xs) = [x] ++ take' (n - 1) xs
+
+drop' :: Int -> [a] -> [a]
+drop' 0 xs = xs
+drop' n [] = []
+drop' n (x:xs) = drop' (n - 1) xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f [] = []
+filter' f (x:xs) = if f x
+		   then [x] ++ filter' f xs
+		   else filter' f xs  
+
+foldl' :: (a -> b -> a) -> a -> [b] -> a
+foldl' f z [] = z
+foldl' f z (x:xs) = foldl' f (f z x) xs
+
+concat' :: [a] -> [a] -> [a]
+concat' a [] = a
+concat' a (x:xs)= concat' (a ++ [x]) xs
+
+quickSort' :: Ord a => [a] -> [a]
+quickSort' [] = []
+quickSort' a = let x0 = head' a
+		   l = quickSort' (filter' (< x0) a)
+		   m = filter' (== x0) a
+		   r = quickSort' (filter' (> x0) a)
+	       in concat' (concat' l m) r
