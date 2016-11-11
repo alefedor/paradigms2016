aad16ef124bb7141a4053d0eba337104539fb5cf
studentfedorov <fedorov2602>
2016-11-11 22:18:03 +0300

    ++ is replaced with :

--- Homework_8/Fp1.hs
+++ Homework_8/Fp1.hs
@@ -8,7 +8,7 @@ tail' (x:xs) = xs
 take' :: Int -> [a] -> [a]
 take' 0 xs = []
 take' n [] = []
-take' n (x:xs) = [x] ++ take' (n - 1) xs
+take' n (x:xs) = x : take' (n - 1) xs
 
 drop' :: Int -> [a] -> [a]
 drop' 0 xs = xs
@@ -18,7 +18,7 @@ drop' n (x:xs) = drop' (n - 1) xs
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' f [] = []
 filter' f (x:xs) = if f x
-		   then [x] ++ filter' f xs
+		   then x : filter' f xs
 		   else filter' f xs  
 
 foldl' :: (a -> b -> a) -> a -> [b] -> a
@@ -26,8 +26,8 @@ foldl' f z [] = z
 foldl' f z (x:xs) = foldl' f (f z x) xs
 
 concat' :: [a] -> [a] -> [a]
-concat' a [] = a
-concat' a (x:xs)= concat' (a ++ [x]) xs
+concat' [] a = a
+concat' (x:xs) a = x : (concat' xs a)
 
 quickSort' :: Ord a => [a] -> [a]
 quickSort' [] = []
